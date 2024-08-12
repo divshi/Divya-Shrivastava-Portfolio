7135ef5211dbce214c2a1ab5db6ad24d00388299
Divya Shrivastava <shridivya893@gmail.com>
2024-08-13 00:52:37 +0530

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/about';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import About from './components/About';
 
 const App = () => {
   return (
